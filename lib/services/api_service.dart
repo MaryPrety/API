67dc7c8d6db973f0a32a1381e15b54a56df85ed9
MaryPrety <116995383+MaryPrety@users.noreply.github.com>
2024-10-28 13:57:46 +0300

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,7 +8,6 @@ class ApiService {
   Future<List<MangaItem>> fetchProducts() async {
     try {
       final response = await _dio.get('$baseUrl/mangaItems');
-
       if (response.statusCode == 200) {
         List<MangaItem> products = (response.data as List)
             .map((item) => MangaItem.fromJson(item))
@@ -31,7 +30,8 @@ class ApiService {
         options: Options(headers: {'Content-Type': 'application/json'}),
       );
 
-      if (response.statusCode == 200) {
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        print("Created new MangaItem: ${response.data}");
         return MangaItem.fromJson(response.data);
       } else {
         throw Exception('Failed to create product: ${response.statusCode} - ${response.data}');
@@ -45,15 +45,36 @@ class ApiService {
   Future<void> deleteProduct(int id) async {
     try {
       final response = await _dio.delete('$baseUrl/mangaItems/delete/$id');
-
-      if (response.statusCode == 404) {
+      if (response.statusCode == 204) {
+        print("Deleted MangaItem with ID: $id");
+      } else if (response.statusCode == 404) {
         throw Exception('Product not found: ${response.statusCode} - ${response.data}');
-      } else if (response.statusCode != 204) {
+      } else {
         throw Exception('Failed to delete product: ${response.statusCode} - ${response.data}');
       }
     } catch (e) {
       print('Error deleting product: $e');
       throw Exception('Error deleting product: $e');
+    }
+  }
+
+  Future<MangaItem> updateProduct(MangaItem item) async {
+    try {
+      final response = await _dio.put(
+        '$baseUrl/mangaItems/update/${item.id}',
+        data: item.toJson(),
+        options: Options(headers: {'Content-Type': 'application/json'}),
+      );
+
+      if (response.statusCode == 200) {
+        print("Updated MangaItem with ID: ${item.id}");
+        return MangaItem.fromJson(response.data);
+      } else {
+        throw Exception('Failed to update product: ${response.statusCode} - ${response.data}');
+      }
+    } catch (e) {
+      print('Error updating product: $e');
+      throw Exception('Error updating product: $e');
     }
   }
 }
